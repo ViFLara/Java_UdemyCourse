45b2b96e4447ef5a5b3e8f4dab3a73a8f67c9e6e
ViFLara <viviflara@gmail.com>
2021-03-10 11:02:42 -0300

    Exception

---
+++ src/main/java/udemy/course/exercise14/model/exception/DomainException.java
@@ -0,0 +1,10 @@
+package udemy.course.exercise14.model.exception;
+
+public class DomainException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public DomainException(String msg) {
+        super(msg);
+    }
+}
+
