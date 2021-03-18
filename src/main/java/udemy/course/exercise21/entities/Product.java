7c4bc2724f52d6098c429f251c452ebd297f597b
ViFLara <viviflara@gmail.com>
2021-03-18 13:00:31 -0300

    Generics

---
+++ src/main/java/udemy/course/exercise21/entities/Product.java
@@ -0,0 +1,41 @@
+package udemy.course.exercise21.entities;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Product implements Comparable<Product> {
+
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return  name + ", " +
+                String.format("%.2f", price);
+    }
+
+    @Override
+    public int compareTo(@NotNull Product other) {
+        return price.compareTo(other.getPrice());
+    }
+}
